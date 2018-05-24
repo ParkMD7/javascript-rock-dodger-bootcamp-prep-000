5d955186cf7f621a491e1381e31f8bc6307b2ead
Parker Dinsmore <parkermdinsmore@gmail.com>
2018-05-24 19:30:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,12 +29,12 @@ function checkCollision(rock) {
     const dodgerLeftEdge = positionToInteger(DODGER.style.left)
 
     // FIXME: The DODGER is 40 pixels wide -- how do we get the right edge?
-    const dodgerRightEdge = 0;
+    const dodgerRightEdge = dodgerLeftEdge + 40;
 
     const rockLeftEdge = positionToInteger(rock.style.left)
 
     // FIXME: The rock is 20 pixel's wide -- how do we get the right edge?
-    const rockRightEdge = 0;
+    const rockRightEdge = rockLeftEdge + 20;
 
     if (false /**
                * Think about it -- what's happening here?
@@ -45,7 +45,11 @@ function checkCollision(rock) {
                *    and the rock's right edge is < the DODGER's right edge;
                * 3. The rock's left edge is < the DODGER's right edge,
                *    and the rock's right edge is > the DODGER's right edge
-               */) {
+               */) 
+     (rockLeftEdge <= dodgerLeftEdge && rockRightEdge >= dodgerLeftEdge)
+     (rockLeftEdge >= dodgerLeftEdge && rockRightEdge <= dodgerRightEdge)
+     (rockLeftEdge <= dodgerRightEdge && rockRightEdge >= dodgerRightEdge)
+               {
       return true
     }
   }
@@ -67,6 +71,7 @@ function createRock(x) {
    * it to GAME and move it downwards.
    */
 
+GAME.appendChild(rock)
 
   /**
    * This function moves the rock. (2 pixels at a time
@@ -80,18 +85,32 @@ function createRock(x) {
      * we should call endGame()
      */
 
+if(checkCollision(rock)) {
+  return endGame()
+}
+
     /**
      * Otherwise, if the rock hasn't reached the bottom of
      * the GAME, we want to move it again.
      */
 
+else if (top < GAME_HEIGHT) {
+  window.requestAnimationFrame(moveRock)
+}
+
     /**
      * But if the rock *has* reached the bottom of the GAME,
      * we should remove the rock from the DOM
      */
+
+else {
+  rock.remove()
+}
+
   }
 
   // We should kick of the animation of the rock around here
+  window.requestAnimationFrame(moveRock)
 
   // Add the rock to ROCKS so that we can remove all rocks
   // when there's a collision
